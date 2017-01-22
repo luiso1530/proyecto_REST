1146fe364412fad482208317795fed8688366a92
luiso1530 <merayo05@gmail.com>
2017-01-21 22:29:50 -0600

    Parte de usuarios

---
+++ proyecto_restaurantes/sistemareservas/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SistemareservasConfig(AppConfig):
+    name = 'sistemareservas'
---
+++ proyecto_restaurantes/sistemareservas/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+class Restaurantes(models.Model):
+    nombre = models.CharField(max_length=20)
+    descripcion = models.CharField(max_length=50)
+    numero_tel = models.IntegerField()
+    
+class reservaciones(models.Model):
+     Opciones_Horas = (
+        ('1:00 pm', 'una'),
+        ('2:00 pm', 'dos'),
+        ('2:00 pm', 'tres'),
+    )
+    cliente = models.CharField(max_length=20)
+    fecha = models.ForeignKey()#aqui poner referencia a un modelo de calendario
+    cantidad_personas = models.IntegerField()
+    hora = models.CharField(max_length=1, choices=Opciones_Horas)
+
+
+
+
+
+
