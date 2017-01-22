b524b7b8e7f73fdcdf0a8418a70c4d89a550daf9
Kmena <kmena281@gmail.com>
2017-01-22 13:25:35 -0600

    actualizacion de users

--- restaurant_app/users/urls.py
+++ restaurant_app/users/urls.py
@@ -5,5 +5,8 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    
+    url(r'login/$',views.login,name= 'login'),
+    url(r'ingresar/$',views.ingresar,name= 'ingresar'),
+    url(r'registrar/$',views.registrar,name= 'registrar'),
+    url(r'newU/$',views.newU,name= 'newU'),
     ]
--- restaurant_app/users/views.py
+++ restaurant_app/users/views.py
@@ -1,7 +1,40 @@
 from django.http import HttpResponseRedirect, HttpResponse
+from django.template.response import TemplateResponse
 from django.urls import reverse
 from django.shortcuts import get_object_or_404, render
 from django.template import loader
 from django.utils import timezone
+from .models import usuario,owner
 
 # Create your views here.
+
+
+def login(request):
+    return render(request, 'users/login.html')
+
+def registrar(request):
+    return render(request, 'users/registrar.html',)
+
+def newU(request):
+    U= usuario(nombre= request.POST.get("nombre",""),user =request.POST.get("usuario",""),contraseña =request.POST.get("password",""))
+    U.save()
+    return render(request, 'users/login.html')
+               
+def ingresar(request):
+    try:
+        selected_choice = usuario.objects.get(nombre = request.POST.get("nombre",""))
+    except (KeyError, usuario.DoesNotExist):
+        try:
+            selected_choice = usuario.objects.get(user = request.POST.get("usuario",""))
+        except (KeyError, usuario.DoesNotExist):
+            return render(request, 'users/login.html', {
+            'error_message': "usuario no exite.",
+        })
+        else:
+            return render(request, 'users/login.html', {
+            'error_message': "usuario confirmado",
+        })
+    else:
+        return render(request, 'users/login.html', {
+        'error_message': "usuario confirmado",
+        })
