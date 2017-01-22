5f05591130e926769319b552f14faf578a8c2d93
Kmena <kmena281@gmail.com>
2017-01-21 19:38:14 -0600

    proyecto

---
+++ restaurant_app/users/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class usuario(models.Model):
+    nombre = models.CharField(max_length=20)
+    user= models.CharField(max_length=20)
+    contraseña = models.CharField(max_length=20)
+
+class owner(models.Model):
+    nombre = models.CharField(max_length=20)
+    user= models.CharField(max_length=20)
+    contraseña = models.CharField(max_length=20)
---
+++ restaurant_app/users/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
+from django.utils import timezone
+
+# Create your views here.
