6a09d45405cfa9c44f8680df14925d24dd1a43b6
Kmena <kmena281@gmail.com>
2017-01-29 19:19:26 -0600

    modificacion del owner

--- restaurant_app/owner/views.py
+++ restaurant_app/owner/views.py
@@ -6,14 +6,17 @@ from django.template import loader
 from django.utils import timezone
 from users.models import usuario,owner
 from sistema_de_reservas.models import Restaurantes, reservaciones
+import time
 # Create your views here.
 
 def principal(request, own):
     latest_question_list = owner.objects.get(user = own)
-    restaurante = Restaurantes.objects.get(pk = 1)#context.restaurante)
+    restaurante = Restaurantes.objects.get(pk = latest_question_list.restaurante)
+    minimo= time.strftime("%Y-%m-%d")
     context = {
         'usuario': latest_question_list,
         'restaurante' : restaurante,
+        'fecmin': minimo,
     }
     return render(request, 'owner/principal.html', context)
     #template = loader.get_template('owner/principal.html')
@@ -30,3 +33,12 @@ def solicitudes(request,own,pk):
     o = owner.objects.get(user = own)
     peticiones = reservaciones.objects.get(pk=pk)
     return render(request, 'owner/peticion.html', {'peticion': peticiones,'own':o})
+
+def solicitud_fecha(request,own):
+    o = owner.objects.get(user = own)
+    rest = Restaurantes.objects.get(pk = o.restaurante)
+    peticiones = reservaciones.objects.filter(restaurantes = rest.nombre)
+    fecha = request.POST.get("fecha","")
+    minimo= time.strftime("%Y/%m/%d")
+    output = peticiones.filter(dia = fecha).order_by('-hora')
+    return render(request, 'owner/peticiones.html', {'peticion': output,'own':o})
